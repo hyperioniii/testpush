076c2c314295ae97775cc7cafa2ec4960cd59fbd
LinhNH <linhnh@runsystem.net>
2015-11-10 00:33:45 +0700

    abc

--- app/src/main/java/com/example/linhnh/myapplication/fragment/FragmentEditImage.java
+++ app/src/main/java/com/example/linhnh/myapplication/fragment/FragmentEditImage.java
@@ -1,18 +1,17 @@
 package com.example.linhnh.myapplication.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.linhnh.myapplication.R;
-import com.example.linhnh.myapplication.activity.MainActivity;
-import com.example.linhnh.myapplication.callback.OnHeaderIconClickListener;
 import com.example.linhnh.myapplication.constant.HeaderIconOption;
 import com.example.linhnh.myapplication.eventbus.MainScreenSettingEvent;
-import com.example.linhnh.myapplication.util.DebugLog;
 import com.larswerkman.lobsterpicker.adapters.BitmapColorAdapter;
 import com.larswerkman.lobsterpicker.sliders.LobsterOpacitySlider;
 import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider;
+
 
 import butterknife.InjectView;
 import de.greenrobot.event.EventBus;
@@ -34,6 +33,9 @@ public class FragmentEditImage extends BaseFragment {
 
     @InjectView(R.id.shadeslider)
     LobsterShadeSlider shadeSlider;
+
+    @InjectView(R.id.img_edit)
+    ImageView editImg;
 
     public static FragmentEditImage intantce() {
         FragmentEditImage fm = new FragmentEditImage();
@@ -61,6 +63,22 @@ public class FragmentEditImage extends BaseFragment {
     @Override
     protected void initData() {
         shadeSlider.setColorAdapter(new BitmapColorAdapter(getActivity(), R.drawable.default_shader_pallete));
+        setColor();
+    }
+
+    public void setColor(){
+//        editImg.setColorFilter(opacitySlider.getColor());
+//        editImg.setColorFilter(Color.RED, PorterDuff.Mode.LIGHTEN);
+
+
+//        editImg.setImageBitmap(bm2);
+//        Glide.with(getActivity()).load(R.drawable.images).into(editImg);
+
+
+//        int mColor = (int) Math.floor(Math.random() * mColors.length);
+//        bitmapOrg.setColorFilter(mColors[mColor], PorterDuff.Mode.MULTIPLY);
+//        imageView.setImageDrawable(bitmapOrg);
+//        imageView.invalidate();
     }
 
 }
